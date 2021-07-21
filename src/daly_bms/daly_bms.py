3639224b52c47e919eea791639d899cc2d44c3a4
Guillem Gari <ggari@robotnik.es>
2021-07-21 09:47:52 +0200

    minor format fix

--- src/daly_bms/daly_bms.py
+++ src/daly_bms/daly_bms.py
@@ -15,25 +15,25 @@ class DalyBMS(RComponent):
         self._last_battery_state = 'Unknown'
         self._time_init_charging = rospy.Time.now()
         self._last_discharge_value = 3.0
-        
-    
+
+
     def ros_read_params(self):
         RComponent.ros_read_params(self)
-        
+
         self._port = rospy.get_param('~serial_port', "/dev/ttyUSB_BMS")
-    
+
     def ros_setup(self):
         self._battery_status_pub = rospy.Publisher("~data", BatteryStatus, queue_size=10)
         self._reading_timer = threading.Timer(self._publish_state_timer, self.read)
-        
+
         RComponent.ros_setup(self)
-    
+
     def setup(self):
         self._driver.connect(self._port)
         self._reading_timer.start()
 
         RComponent.setup(self)
-    
+
     def shutdown(self):
         self._reading_timer.cancel()
 
@@ -43,7 +43,7 @@ class DalyBMS(RComponent):
         self._battery_status_pub.unregister()
 
         RComponent.ros_shutdown(self)
-        
+
     def read(self):
         data = self._driver.get_soc()
         #print(self._driver.get_all())
@@ -53,14 +53,14 @@ class DalyBMS(RComponent):
         self._battery_status.current = data['current']
 
         data = self._driver.get_mosfet_status()
-        
+
         if data['mode'] == 'discharging':
             self._battery_status.is_charging = False
             self._battery_status.time_charging = 0
             self._last_discharge_value = self._battery_status.current
-        
+
         elif data['mode'] == 'charging':
-        
+
             if self._last_battery_state == 'Unknown' or self._last_battery_state == 'discharging':
                 self._time_init_charging = rospy.Time.now().secs
 
@@ -77,8 +77,8 @@ class DalyBMS(RComponent):
 
         self._reading_timer = threading.Timer(self._publish_state_timer, self.read)
         self._reading_timer.start()
-        
-    
+
+
     def ros_publish(self):
         self._battery_status_pub.publish(self._battery_status)
 
