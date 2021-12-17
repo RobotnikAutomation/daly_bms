7fd69f3d0062194ca5fe03851c94b0f895218c21
Guillem Gari <ggari@robotnik.es>
2021-12-17 14:06:14 +0100

    corrected packages for container

---
+++ containers/entrypoint/ros_entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# setup ros environment
+source "/opt/ros/$ROS_DISTRO/setup.bash"
+#source "$RBK_CATKIN_PATH/install/setup.bash"
+source "$RBK_CATKIN_PATH/devel/setup.bash"
+exec "$@"
