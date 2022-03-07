488e3f4503cab3f609dca438c305f78d1af15ef2
alvarovillena <avillena@robotnik.es>
2022-03-07 13:12:59 +0100

    Prevent errors when driver fails to return data + Add RepeatTimer class

--- src/daly_bms/daly_bms.py
+++ src/daly_bms/daly_bms.py
@@ -6,6 +6,11 @@ from robotnik_msgs.msg import BatteryStatus
 
 from dalybms import DalyBMS as DalyBMSDriver
 
+class RepeatTimer(threading.Timer):
+    def run(self):
+        while not self.finished.wait(self.interval):
+            self.function(*self.args, **self.kwargs)
+
 class DalyBMS(RComponent):
     def __init__(self):
         RComponent.__init__(self)
@@ -24,7 +29,7 @@ class DalyBMS(RComponent):
 
     def ros_setup(self):
         self._battery_status_pub = rospy.Publisher("~data", BatteryStatus, queue_size=10)
-        self._reading_timer = threading.Timer(self._publish_state_timer, self.read)
+        self._reading_timer = RepeatTimer(self._publish_state_timer, self.read)
 
         RComponent.ros_setup(self)
 
@@ -45,20 +50,24 @@ class DalyBMS(RComponent):
         RComponent.ros_shutdown(self)
 
     def read(self):
-        data = self._driver.get_soc()
+        soc_data = self._driver.get_soc()
+        mosfet_data = self._driver.get_mosfet_status()
+        cells_data = self._driver.get_cell_voltages()
 
-        self._battery_status.level = data['soc_percent']
-        self._battery_status.voltage = data['total_voltage']
-        self._battery_status.current = data['current']
+        if soc_data == False or mosfet_data == False or cells_data == False:
+            rospy.logwarn("Skipping current read cycle: Driver failed to return data")
+            return
 
-        data = self._driver.get_mosfet_status()
+        self._battery_status.level = soc_data['soc_percent']
+        self._battery_status.voltage = soc_data['total_voltage']
+        self._battery_status.current = soc_data['current']
 
-        if data['mode'] == 'discharging':
+        if mosfet_data['mode'] == 'discharging':
             self._battery_status.is_charging = False
             self._battery_status.time_charging = 0
             self._last_discharge_value = self._battery_status.current
 
-        elif data['mode'] == 'charging' or data['mode'] == 'stationary':
+        elif mosfet_data['mode'] == 'charging' or mosfet_data['mode'] == 'stationary':
 
             if self._last_battery_state == 'Unknown' or self._last_battery_state == 'discharging':
                 self._time_init_charging = rospy.Time.now().secs
@@ -72,17 +81,14 @@ class DalyBMS(RComponent):
 
         # _last_discharge_value is negative in certain cases
         if self._last_discharge_value != 0:
-            remaining_hours = round(data['capacity_ah']/self._last_discharge_value, 0)
+            remaining_hours = round(mosfet_data['capacity_ah']/self._last_discharge_value, 0)
         else:
             remaining_hours = 0
 
         self._battery_status.time_remaining = max(0, int(remaining_hours)*60) # remaining_hours is negative in certain cases
-        self._last_battery_state = data['mode']
+        self._last_battery_state = mosfet_data['mode']
 
-        self._battery_status.cell_voltages = list(self._driver.get_cell_voltages().values())
-
-        self._reading_timer = threading.Timer(self._publish_state_timer, self.read)
-        self._reading_timer.start()
+        self._battery_status.cell_voltages = list(cells_data.values())
 
 
     def ros_publish(self):
