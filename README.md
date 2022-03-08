217783f7c4983d7f214dce4f4595ed714a8e393e
Álvaro Villena <avillena@robotnik.es>
2022-03-08 14:04:46 +0100

    Decouple driver (#1) + Multiple bugfixes
    * Decouple driver from ROS node
    
    * bugfix: Return cell voltages instead of index
    
    * Do not add errors field to ROS publisher
    
    * Set /dev/ttyUSB_BMS as the default port for the launch file too
    
    * Update README.md
    
    * Check if mosfet mode is stationary and do the same than in charging mode
    
    * Ensure that remaining hours is always positive
    
    * Check if last_discharge_value is 0 to avoid division by 0
    
    * Prevent errors when driver fails to return data + Add RepeatTimer class

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
 # Daly BMS
 
-ROS package based on [dreadnought](https://github.com/dreadnought) module for reading data from Daly BMS devices and adapted to Robotnik messages and RComponent structure.
+ROS package that uses [python-daly-bms driver](https://github.com/dreadnought/python-daly-bms) data to read information from Daly BMS devices and publishes it using robotnik_msgs/BatteryStatus message type.
 
 ## Installation
 
 ```
+sudo apt install python3-rospkg-modules
+```
+
+Install [python-daly-bms driver](https://github.com/dreadnought/python-daly-bms) by following the instructions provided in the README.md file. This is a python library independent from ROS, so there is no need to install it in the catkin workspace.
+
+```
 git clone https://github.com/RobotnikAutomation/daly_bms.git
+```
+
+```
+catkin build daly_bms
 ```
 
 ### Dependencies
@@ -17,4 +27,8 @@ pip3 install pyserial
 
 ## Parameters
 
-- **port** (String, /dev/ttyUSB0): Serial port of the device
+- **port** (String, /dev/ttyUSB_BMS): Serial port of the device
+
+## Additional information
+
+This package includes an udev rule to find the USB device using its model and vendor identifiers. This is useful to bind the device to a specific port, independently of the actual physical port where the device is plugged. To add this udev rule to your system, just copy it into your /etc/udev/rules.d directory.
--- src/daly_bms/daly_bms.py
+++ src/daly_bms/daly_bms.py
@@ -4,7 +4,12 @@ import rospy
 from rcomponent.rcomponent import RComponent
 from robotnik_msgs.msg import BatteryStatus
 
-from daly_bms_driver import DalyBMSDriver
+from dalybms import DalyBMS as DalyBMSDriver
+
+class RepeatTimer(threading.Timer):
+    def run(self):
+        while not self.finished.wait(self.interval):
+            self.function(*self.args, **self.kwargs)
 
 class DalyBMS(RComponent):
     def __init__(self):
@@ -15,25 +20,25 @@ class DalyBMS(RComponent):
         self._last_battery_state = 'Unknown'
         self._time_init_charging = rospy.Time.now()
         self._last_discharge_value = 3.0
-        
-    
+
+
     def ros_read_params(self):
         RComponent.ros_read_params(self)
-        
+
         self._port = rospy.get_param('~serial_port', "/dev/ttyUSB_BMS")
-    
+
     def ros_setup(self):
         self._battery_status_pub = rospy.Publisher("~data", BatteryStatus, queue_size=10)
-        self._reading_timer = threading.Timer(self._publish_state_timer, self.read)
-        
+        self._reading_timer = RepeatTimer(self._publish_state_timer, self.read)
+
         RComponent.ros_setup(self)
-    
+
     def setup(self):
         self._driver.connect(self._port)
         self._reading_timer.start()
 
         RComponent.setup(self)
-    
+
     def shutdown(self):
         self._reading_timer.cancel()
 
@@ -43,24 +48,27 @@ class DalyBMS(RComponent):
         self._battery_status_pub.unregister()
 
         RComponent.ros_shutdown(self)
-        
+
     def read(self):
-        data = self._driver.get_soc()
-        #print(self._driver.get_all())
+        soc_data = self._driver.get_soc()
+        mosfet_data = self._driver.get_mosfet_status()
+        cells_data = self._driver.get_cell_voltages()
 
-        self._battery_status.level = data['soc_percent']
-        self._battery_status.voltage = data['total_voltage']
-        self._battery_status.current = data['current']
+        if soc_data == False or mosfet_data == False or cells_data == False:
+            rospy.logwarn("Skipping current read cycle: Driver failed to return data")
+            return
 
-        data = self._driver.get_mosfet_status()
-        
-        if data['mode'] == 'discharging':
+        self._battery_status.level = soc_data['soc_percent']
+        self._battery_status.voltage = soc_data['total_voltage']
+        self._battery_status.current = soc_data['current']
+
+        if mosfet_data['mode'] == 'discharging':
             self._battery_status.is_charging = False
             self._battery_status.time_charging = 0
             self._last_discharge_value = self._battery_status.current
-        
-        elif data['mode'] == 'charging':
-        
+
+        elif mosfet_data['mode'] == 'charging' or mosfet_data['mode'] == 'stationary':
+
             if self._last_battery_state == 'Unknown' or self._last_battery_state == 'discharging':
                 self._time_init_charging = rospy.Time.now().secs
 
@@ -71,16 +79,17 @@ class DalyBMS(RComponent):
 
             self._battery_status.time_charging = elapsed_time
 
-        remaining_hours = round(data['capacity_ah']/self._last_discharge_value, 0)
-        self._battery_status.time_remaining = int(remaining_hours)*60
-        self._last_battery_state = data['mode']
+        # _last_discharge_value is negative in certain cases
+        if self._last_discharge_value != 0:
+            remaining_hours = round(mosfet_data['capacity_ah']/self._last_discharge_value, 0)
+        else:
+            remaining_hours = 0
 
-        self._reading_timer = threading.Timer(self._publish_state_timer, self.read)
-        self._reading_timer.start()
-        
-    
+        self._battery_status.time_remaining = max(0, int(remaining_hours)*60) # remaining_hours is negative in certain cases
+        self._last_battery_state = mosfet_data['mode']
+
+        self._battery_status.cell_voltages = list(cells_data.values())
+
+
     def ros_publish(self):
         self._battery_status_pub.publish(self._battery_status)
-
-
-
