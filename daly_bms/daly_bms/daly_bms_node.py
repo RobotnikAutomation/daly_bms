60d50f1b08e52d02fed7673b3bc85c401e224534
Guillem Gari <ggari@robotnik.es>
2024-09-02 11:17:20 +0200

    Corrected style for print

--- daly_bms/daly_bms/daly_bms_node.py
+++ daly_bms/daly_bms/daly_bms_node.py
@@ -7,7 +7,9 @@ from daly_bms import DalyBMS
 def main(args=None):
     rclpy.init(args=args)
     daly_bms = DalyBMS()
-    daly_bms.get_logger().info(f"Starting Daly BMS Node{ daly_bms.get_name() }")
+    daly_bms.get_logger().info(
+        f"Starting Daly BMS Node{ daly_bms.get_name() }"
+    )
     daly_bms.setup()
     rclpy.spin(daly_bms)
     daly_bms.destroy_node()
